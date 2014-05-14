27d27c3dab7122e74dedfe51bf1de5c059e3a4c6
Mark Kostick <markk@Marks-MacBook-Pro.local>
2014-05-14 13:11:44 -0400

    Allow Pronto::Rubocop instance to be configured with a block
    Pronto does not expose the instances of its runners, so there was no way to pass
    configuration options to them.  This adds a class method to define a block to be
    evaluated in the context of a new Pronto::Rubocop instance after it's
    initialized.

--- spec/pronto/rubocop_spec.rb
+++ spec/pronto/rubocop_spec.rb
@@ -28,5 +28,44 @@ module Pronto
         end
       end
     end
+
+    describe '#initialize' do
+      let(:inspector) { double('FileInspector') }
+      let(:config_store) { double('ConfigStore') }
+
+      before(:each) do
+        ::Rubocop::FileInspector.stub(:new).and_return(inspector)
+        ::Rubocop::ConfigStore.stub(:new).and_return(config_store)
+      end
+
+      it 'creates object instances' do
+        ::Rubocop::FileInspector.should_receive(:new).with({})
+        ::Rubocop::ConfigStore.should_receive(:new).with(no_args)
+
+        rubocop
+      end
+
+      it 'assigns instance variables' do
+        rubocop.instance_variable_get(:@inspector).should == inspector
+        rubocop.instance_variable_get(:@config_store).should == config_store
+      end
+
+      context 'when an instance configure block is used' do
+        let(:options) { double('options') }
+
+        before(:each) do
+          block_options = options
+          ::Pronto::Rubocop.instance_configure do
+            @config_store.options = block_options
+          end
+        end
+
+        it 'evaluates the block in the context of the instance' do
+          config_store.should_receive(:options=).with(options)
+
+          rubocop
+        end
+      end
+    end
   end
 end
